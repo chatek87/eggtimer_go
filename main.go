27143b3783d0bbe4934d18f62f4fed8724da4d8c
Charlie Tekulve <54796991+chatek87@users.noreply.github.com>
2024-05-30 14:50:25 -0400

    implement progress bar. NEXT: move progress bar above button

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ type C = layout.Context
 type D = layout.Dimensions
 
 var progress float32
-var progressIncrementer chan float32	// progressIncrementer is the channel into which we send values, in this case of type float32
+var progressIncrementer chan float32 // progressIncrementer is the channel into which we send values, in this case of type float32
 var boiling bool
 
 func main() {
 	// setup a separate channel to provide ticks to increment progress
 	progressIncrementer = make(chan float32)
 	go func() {
-		for {
+		for { // every 1/25th of a second the number 0.004 is injected into the channel
 			time.Sleep(time.Second / 25)
 			progressIncrementer <- 0.004
 		}
@@ -52,6 +52,16 @@ func draw(w *app.Window) error {
 
 	// th defines the material design style
 	th := material.NewTheme()
+
+	// Start the goroutine that listens for events in the incrementer channel
+	go func() {
+		for p := range progressIncrementer {
+			if boiling && progress < 1 {
+				progress += p
+				w.Invalidate()
+			}
+		}
+	}()
 
 	// listen for events in the window 	(this is the EVENT LOOP)
 	for {
